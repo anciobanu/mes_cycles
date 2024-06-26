f80906a239eccd3f813c25007942f5f22d5da712
Andrei <85348711+anciobanu@users.noreply.github.com>
2024-06-26 20:37:22 +0200

    First version of markers in calendar

--- lib/src/widget/calendar.dart
+++ lib/src/widget/calendar.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mes_cycles/src/model/journee.dart';
@@ -24,6 +26,15 @@ class _CalendarState extends State<Calendar> {
     super.initState();
   }
 
+  List<Journee> _isDaySet(DateTime dateTime) {
+    final String dayKey = DateTime(dateTime.year, dateTime.month, dateTime.day).toString();
+    final List<Journee> joursSaisis = [];
+    if(_cycles.containsKey(dayKey)) {
+      joursSaisis.add(Journee(_cycles.get(dayKey)!.journee, _cycles.get(dayKey)!.commentaires, _cycles.get(dayKey)!.flux, _cycles.get(dayKey)!.transit, _cycles.get(dayKey)!.ballonnements, _cycles.get(dayKey)!.douleurs, _cycles.get(dayKey)!.jambesLourdes, _cycles.get(dayKey)!.forme, _cycles.get(dayKey)!.libido, _cycles.get(dayKey)!.stress));
+    }
+    return joursSaisis;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<Box>(
@@ -41,9 +52,18 @@ class _CalendarState extends State<Calendar> {
                       focusedDay: daySelected,
                       firstDay: DateTime.utc(2024, 1, 1),
                       lastDay: DateTime.now(),
-                      currentDay: daySelected, //DateTime.now(),
+                      currentDay: daySelected,
                       calendarFormat: CalendarFormat.month,
                       startingDayOfWeek: StartingDayOfWeek.monday,
+                      selectedDayPredicate: (day) {
+                        return isSameDay(daySelected, day);
+                        /*if(_cycles.containsKey(DateTime(day.year, day.month, day.day).toString())) {
+                          return true;
+                        }
+                        else {
+                          return false;
+                        }*/
+                      },
                       headerStyle: HeaderStyle(
                         formatButtonVisible: false,
                         titleCentered: true,
@@ -67,6 +87,108 @@ class _CalendarState extends State<Calendar> {
                           daySelected = selectedDay;
                         });
                       },
+                      eventLoader: _isDaySet,
+                      calendarBuilders: CalendarBuilders(
+                        defaultBuilder: (context, day, focusedDay) {
+                          if (_cycles.containsKey(DateTime(day.year, day.month, day.day).toString())) {
+                            if (_cycles.get(DateTime(day.year, day.month, day.day).toString())!.flux != 0) {
+                              return Center(
+                                child: Text(day.day.toString(),
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.bold)),
+                            );
+                            } else {
+                              return Center(
+                                child: Text(day.day.toString(),
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                          color: Theme.of(context).colorScheme.onBackground,
+                                          fontWeight: FontWeight.bold)),
+                            );
+                            }
+                          }
+                          return null;
+                        },
+                        selectedBuilder: (context, day, focusedDay) {
+                        if (_cycles.containsKey(DateTime(day.year, day.month, day.day).toString())) {
+                          if (_cycles.get(DateTime(day.year, day.month, day.day).toString())!.flux !=0) {
+                            return Container(
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.secondaryContainer,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(day.day.toString(),
+                                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                            color: Colors.red,
+                                            fontWeight: FontWeight.bold)),
+                              ),
+                            );
+                          } else {
+                            return Container(
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.secondaryContainer,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(day.day.toString(),
+                                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                            color: Theme.of(context).colorScheme.onBackground,
+                                            fontWeight: FontWeight.bold)),
+                              ),
+                            );
+                          }
+                        }
+                        return Container(
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.secondaryContainer,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Text(day.day.toString(),
+                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                        color: Theme.of(context).colorScheme.onBackground)),
+                          ),
+                        );
+                      },
+                        markerBuilder: (context, day, events) {
+                          if (!_cycles.containsKey(DateTime(day.year, day.month, day.day).toString())) {
+                            return const SizedBox();
+                          } else {
+                            Journee journee = _cycles.get(DateTime(day.year, day.month, day.day).toString())!;
+                            double counter = 0;
+                            if (journee.transit == 1 || journee.transit == 5) {
+                              counter = counter + 2;
+                            }
+                            if (journee.ballonnements >= 2) {
+                              counter = counter + 2;
+                            }
+                            if (journee.douleurs >= 2) {
+                              counter = counter + 2;
+                            }
+                            if(journee.jambesLourdes >= 2){
+                              counter = counter + 2;
+                            }
+                            if(journee.forme <= 3 && journee.forme != 0) {
+                              counter = counter + 2;
+                            }
+                            return Container(
+                                margin: const EdgeInsets.only(top: 20),
+                                padding: const EdgeInsets.all(1),
+                                child: Container(
+                                  height: 5,
+                                  width: counter,
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      color: Colors.red),
+                                ),
+                              );
+                            }
+                          }
+                      ),
                     ),
                     const SizedBox(height: 16,),
                     MaJournee(cycles: _cycles, daySelected: daySelected,),
